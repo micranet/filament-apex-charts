3bc7d07395c2319935a98b7e4515d1ac922064f2
micranet <micranet@users.noreply.github.com>
2025-07-14 17:22:08 +0200

    filter badge style

--- resources/views/widgets/components/filter-form.blade.php
+++ resources/views/widgets/components/filter-form.blade.php
@@ -19,7 +19,7 @@
             @if ($indicatorsCount > 0)
                 <div class="absolute start-full top-0 z-10 -ms-1 -translate-x-1/2 rounded-md bg-white dark:bg-gray-900">
                     <div style="--c-50:var(--primary-50);--c-300:var(--primary-300);--c-400:var(--primary-400);--c-600:var(--primary-600);"
-                        class="fi-badge flex items-center justify-center gap-x-1 whitespace-nowrap rounded-md  text-xs font-medium ring-1 ring-inset px-0.5 min-w-[theme(spacing.4)] tracking-tighter bg-custom-50 text-custom-600 ring-custom-600/10 dark:bg-custom-400/10 dark:text-custom-400 dark:ring-custom-400/30">
+                        class="fi-badge fi-size-xs flex items-center justify-center gap-x-1 whitespace-nowrap rounded-md  text-xs font-medium ring-1 ring-inset px-0.5 min-w-[theme(spacing.4)] tracking-tighter bg-custom-50 text-custom-600 ring-custom-600/10 dark:bg-custom-400/10 dark:text-custom-400 dark:ring-custom-400/30">
 
                         <span>
                             {{ $indicatorsCount }}
